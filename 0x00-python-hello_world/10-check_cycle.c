c1c258e87faa07f840fb375bc82223ceaf284d78
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-01-18 01:38:39 +0200

    m

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,21 +2,47 @@
 #include <stdlib.h>
 #include "lists.h"
 
+int search_list(listint_t *list, listint_t *original, int i)
+{
+	listint_t *temp;
+	int j;
+
+	j = 0;
+	temp = original;
+
+	while (1)
+	{
+		if (temp == list)
+		{
+			if (j < i)
+				return (1);
+			else
+				break;
+		}
+		j++;
+		temp = temp->next;
+	}
+	return (0);
+
+}
+
 int check_cycle(listint_t *list)
 {
 	listint_t *temp;
+	int i;
 
+	i = 0;
 	if (list == NULL)
 		return (0);
 
 	temp = list;
-	temp = temp->next;
 
 	while (temp != NULL)
 	{
-		if (temp == list)
+		if (search_list(temp, list, i))
 			return (1);
 
+		i++;
 		temp = temp->next;
 	}
 	return (0);
