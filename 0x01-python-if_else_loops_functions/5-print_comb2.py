32c950d15bface7498ac547bd70b7d47afc97ee8
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-01-19 10:00:49 +0200

    m

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{i:02d}".format(i = i))
+        print("{i:02d}".format(i=i))
     else:
-        print("{i:02d}".format(i = i), end=', ')
+        print("{i:02d}".format(i=i), end=', ')
