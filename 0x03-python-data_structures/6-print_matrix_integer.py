91dc4519d2d0d98b0ebacc027f137009d6bd7b29
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-01-24 00:07:25 +0200

    m

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) == 0:
+        print("")
+    for r, row in enumerate(matrix):
+        for i, c in enumerate(row):
+            if i != (len(row) - 1) and c != '-':
+                print("{}".format(c), end=' ')
+            else:
+                print("{}".format(c))
