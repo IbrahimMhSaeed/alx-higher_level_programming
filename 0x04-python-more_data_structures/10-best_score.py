763294b7e2fad7b5215478159138b7589334e082
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-01-28 11:12:21 +0200

    m

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) is 0:
         return (None)
     max_v = float('-inf')
     for key, val in a_dictionary.items():
