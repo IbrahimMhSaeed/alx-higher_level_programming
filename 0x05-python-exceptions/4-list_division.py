32b7f0b8bed69a4fa8fa80cc2cf0b5692b94db06
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-02-06 03:09:50 +0200

    m

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             ans.append(0)
+        finally:
+            div = 0
 
     return (ans)
