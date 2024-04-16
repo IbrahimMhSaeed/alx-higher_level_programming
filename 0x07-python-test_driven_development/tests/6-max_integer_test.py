3cc84efc9b8a8df6b3181cc8f86c6356bb5d69a1
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-04-16 19:46:28 +0200

    m

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This is my unittest module """
+
+
+import unittest
+max_int = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ This is my unittest class """
+
+    def test_len(self):
+        """ test if len of list is 0 """
+        self.assertEqual(max_int(), None)
+        self.assertEqual(max_int([]), None)
+
+
+    def test_max(self):
+        """ test for max value """
+        self.assertEqual(max_int([1, 2, 3]), 3)
+        self.assertEqual(max_int([-1, -2, -3]), -1)
+        self.assertEqual(max_int([123333, 2334, 999]), 123333)
