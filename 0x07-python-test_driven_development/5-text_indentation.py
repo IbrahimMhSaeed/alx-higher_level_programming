dd0184e9c75327400031c424c9429a26de8f5961
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-04-16 18:34:44 +0200

    m

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" text indentation module """
+
+def remove_spaces(start, end, text):
+    while text[start] == " " or text[end] == " ":
+        if text[start] == " ":
+            start += 1
+
+        if text[end] == " ":
+            end -= 1
+
+    return (start, end)
+def text_indentation(text):
+    """ text indentation function """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    start = 0
+    end = 0
+
+    for i, c in enumerate(text):
+        if c in ".?:":
+            end = i - 1
+            if i == (len(text) - 1):
+                end = i
+
+            start, end = remove_spaces(start, end, text)
+            if (end - start) != 0:
+                print(text[start:end+1])
+            else:
+                print("")
+            print("")
+            start = end + 2
+        if i == (len(text) - 1) and c not in ".?:":
+            start, end = remove_spaces(start, end, text)
+            print(text[start:], end="")
