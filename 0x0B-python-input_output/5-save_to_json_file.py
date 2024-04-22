03b94be4b74e074ef8ccb971b449b6a590a8d44b
IbrahimMhSaeed <ibrahimmhsaeed@gmail.com>
2024-04-22 20:38:22 +0200

    m

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" save to json module """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ save to json file """
+    js = json.dumps(my_obj)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(js)
